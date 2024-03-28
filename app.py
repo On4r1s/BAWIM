5f6b9aa3d824a528b55de2eea541b53f49a40974
DarkNikito <nikitos.barbados@gmail.com>
2024-03-28 12:39:49 +0100

    update

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 
+import flask
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
+app.config['UPLOAD_FOLDER'] = '/static/uploads/'
 my_json = json.load(open('settings.json'))
 status = {
     "online": "🟢 Online",
@@ -10,19 +12,53 @@ status = {
     "dnd": "🔴 Do not disturb",
 }
 activity = ['playing', 'streaming', 'listening', 'watching', 'competing']
+langs = {
+    "en": "English",
+    "ru": "Russian"
+}
 
 
-@app.route('/')
-@app.route('/personalize')
-def personalize(menu='personalize'):
-    return render_template('hello.html', menu=menu, settings=my_json['personalize'], status=status, activity=activity)
+@app.get('/')
+@app.get('/personalize')
+def personalize_get(menu='personalize'):
+    return render_template('hello.html', menu=menu, settings=my_json[menu], status=status, activity=activity)
 
 
-@app.route('/settings')
-def settings(menu='settings'):
-    return render_template('hello.html', menu=menu, settings=my_json)
+@app.post('/')
+@app.post('/personalize')
+def personalize_post(menu='personalize'):
+    print(flask.request.form.to_dict())
+    return render_template('hello.html', menu=menu, settings=my_json[menu], status=status, activity=activity)
 
 
-@app.route('/admin')
-def admins(menu='admin_roles'):
-    return render_template('hello.html', menu=menu, settings=my_json)
+@app.get('/settings')
+def settings_get(menu='settings'):
+    return render_template('hello.html', menu=menu, settings=my_json[menu], langs=langs)
+
+
+@app.post('/settings')
+def settings_post(menu='settings'):
+    print(flask.request.form.to_dict())
+    return render_template('hello.html', menu=menu, settings=my_json[menu], langs=langs)
+
+
+@app.get('/commands')
+def commands_get(menu='commands'):
+    return render_template('hello.html', menu=menu, settings=my_json[menu])
+
+
+@app.post('/commands')
+def commands_post(menu='commands'):
+    print(flask.request.form.to_dict())
+    return render_template('hello.html', menu=menu, settings=my_json[menu])
+
+
+@app.get('/admins')
+def admins_get(menu='admins'):
+    return render_template('hello.html', menu=menu, settings=my_json[menu])
+
+
+@app.post('/admins')
+def admins_post(menu='admins'):
+    print(flask.request.form.to_dict())
+    return render_template('hello.html', menu=menu, settings=my_json[menu])
